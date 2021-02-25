e23c10eac99a6674a992730fd9373b30b6ba0655
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2021-02-25 12:59:11 +0300

    Css loader added

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'staff.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
